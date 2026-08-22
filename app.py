764a121db5528f9ecdab6b3e5c9fb2faadf96563
silven-mohan <silven667@gmail.com>
2026-08-22 22:26:34 +0530

    Added: A function to associate a connection to the database.

--- app.py
+++ app.py
@@ -2,3 +2,12 @@
 
 # import modules:
 import mysql.connector
+
+# -------- Database Connection ------------
+def connect_database():
+    return mysql.connector.connect(
+        host="ip",
+        user="root",
+        password="",
+        database="STUDDB"
+    )
